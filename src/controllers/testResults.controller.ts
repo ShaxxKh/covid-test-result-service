5e7c7266659cd8db09910a2f9da0ae601d113922
ShaXx <sop.ugc2@gmail.com>
2021-09-23 18:59:34 +0500

    fix: error logging, correct http response codes, correct response messages

--- src/controllers/testResults.controller.ts
+++ src/controllers/testResults.controller.ts
@@ -29,8 +29,13 @@ interface TestResults {
 
 class TestResultsController {
     saveResults = async ( req: Request, res: Response, next: NextFunction ) => {
+        console.log( "REQUEST HEADER: " + JSON.stringify( req.headers ) );
+        console.log( "REQUEST BODY: " + JSON.stringify( req.body ) );
         const token: any = req.headers[ "x-api-key" ];
         let isAuth: boolean = false;
+        let statusCode: number = 201;
+        let message: any = "All results are added";
+        const transactionBlockedErrCode = '25P02';
 
         try {
             const tokenSearch = await db.query( `
@@ -46,82 +51,42 @@ class TestResultsController {
         }
 
         if ( isAuth ) {
-            const dbRequest = await db.query( `BEGIN`, async ( err: any, result: any ) => {
+            await db.query( `BEGIN`, async ( err: any, result: any ) => {
                 try {
                     for ( const test of req.body ) {
                         const testResultArr = Object.values( test );
 
-
-                        const dbRes = await db.query( `
+                        await db.query( `
                                 INSERT INTO testresults (appointmentId, appointmentStart, appointmentEnd, concernId, concernDescription, locationId, locationName, firstname, lastname, birthDate, eMail, mobileNumber, acceptedPrivacyDeclaration, acceptedTermsOfUse, testId, testResult, testDateTime) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17)
                                 RETURNING *
                             `, [ ...testResultArr ], ( err: any, result: any ) => {
                             if ( err ) {
-                                console.log( "error: " + err );
-                                // throw new Error( err );
-                            } else {
-                                console.log( "good job: " + result );
+                                if ( err.code !== transactionBlockedErrCode ) {
+                                    console.error( "error: " + err );
+                                    statusCode = 400;
+                                    message = err.message;
+                                }
+
                             }
                         } );
-
                     }
-
                     await db.query( "COMMIT" );
-                } catch ( error ) {
-                    console.log( "overall error: " + error );
-                    res.status( 400 ).send( {
-                        message: error
+                } catch ( error: any ) {
+                    console.error( "overall error: " + error );
+                    statusCode = 400;
+                    await db.query( "ROLLBACK" );
+                } finally {
+                    res.status( statusCode ).send( {
+                        message: message
                     } );
-                    await db.query( "END" );
                 }
-
             } );
-            res.status( 201 ).send( {
-                message: "Test results are added"
-            } );
-
-            // const dbRequestPromise: Promise<string> = new Promise<string>( async ( resolve, reject ) => {
-            //     for ( const test of req.body ) {
-            //         const testResultArr = Object.values( test );
-
-
-            //         const dbRes = await db.query( `
-            //                 INSERT INTO testresults (appointmentId, appointmentStart, appointmentEnd, concernId, concernDescription, locationId, locationName, firstname, lastname, birthDate, eMail, mobileNumber, acceptedPrivacyDeclaration, acceptedTermsOfUse, testId, testResult, testDateTime) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17)
-            //                 RETURNING *
-            //             `, [ ...testResultArr ], ( err: any, res: any ) => {
-            //             if ( err ) {
-            //                 reject( err.message );
-
-            //             } else {
-            //                 console.log( "good job" );
-            //             }
-            //         } );
-
-            //     }
-            //     resolve( "" );
-            // } );
-
-            // await db.query( `COMMIT` );
-
-            // await dbRequestPromise.then( () => {
-            //     res.status( 201 ).send( {
-            //         message: "Test results are added"
-            //     } );
-            // } ).catch( async ( error: any ) => {
-            //     await db.query( `ROLLBACK` );
-            //     console.error( "Error occured while posting data to db: " + error );
-            //     res.status( 400 ).send( {
-            //         message: error
-            //     } );
-            // } );
-
 
         } else {
             res.status( 401 ).send( {
                 message: "Wrong token"
             } );
         }
-
     };
 };
 
