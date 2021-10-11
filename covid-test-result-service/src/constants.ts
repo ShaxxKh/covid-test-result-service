f52ac8a5cef981019f076fd7d43838cd58d4d165
ShaXx <sop.ugc2@gmail.com>
2021-10-11 09:58:52 +0500

    fix: download pdf file

--- covid-test-result-service/src/constants.ts
+++ covid-test-result-service/src/constants.ts
@@ -1,5 +1,5 @@
 const constants = {
-    FRONT_URL: "http://localhost:5000/"
+    FRONT_URL: "http://localhost:5000/" // need to change to frontend url after deployment
 };
 
 export default constants;
--- covid-test-result-service/src/controllers/docs.controller.ts
+++ covid-test-result-service/src/controllers/docs.controller.ts
@@ -9,25 +9,9 @@ class DocsController {
         console.log( "REQUEST BODY: " + JSON.stringify( req.body ) );
 
         const pdfPath = await pdfTemplate( { data: req.body } );
-        // res.setHeader( "Content-Disposition", "inline; filename=picture.png" );
-        // await res.writeHead( 200, {
-        //     // 'Content-Length': Buffer.byteLength(  ),
-        //     // 'Content-Type': 'text/plain',
-        //     'Content-Disposition': 'inline; filename="picture.png"'
-        // } );
-        // res.download( pdfPath );
-        // res.sendFile( pdfPath, {
-        // headers: {
-        // 'Content-Type': 'text/plain',
-        // 'Content-Disposition': 'inline; filename="picture.png"';
-        // }
-        // } );
-        var file = fs.createReadStream( pdfPath );
-        var stat = fs.statSync( pdfPath );
-        res.setHeader( 'Content-Length', stat.size );
-        res.setHeader( 'Content-Type', 'text/pdf' );
-        res.setHeader( 'Content-Disposition', 'attachment; filename=quote.pdf' );
-        file.pipe( res );
+
+        var filestream = fs.createReadStream( pdfPath );
+        filestream.pipe( res );
     };
 };
 
