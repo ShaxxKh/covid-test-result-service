56ea81944abcb0427ca990135c1419531e91489a
thezaff <zaffenergizante@gmail.com>
2021-10-15 02:37:29 +0500

    feat: add russian pdf

--- src/db/db.ts
+++ src/db/db.ts
@@ -16,12 +16,10 @@ const dbOptions: dbOptions = {
   host: process.env.PG_HOST || "",
   database: process.env.PG_DATABASE || "",
   user: process.env.PG_USER || "",
-  port: parseInt( process.env.PG_PORT || "5432" ),
+  port: parseInt(process.env.PG_PORT || "5432"),
   password: process.env.PG_PASSWORD || "",
 };
 
-const client = new Client( dbOptions );
-
-
+const client = new Client(dbOptions);
 
 export default client;
--- src/docsTemplates/pdfTemplate.ts
+++ src/docsTemplates/pdfTemplate.ts
@@ -1,54 +1,67 @@
-const pdf = require( 'pdf-creator-node' );
+const pdf = require("pdf-creator-node");
 import fs from "fs";
 import path from "path";
 import constants from "../constants";
-const QR = require( "qrcode-base64" );
+const QR = require("qrcode-base64");
 
-async function pdfTemplate ( data: any ): Promise<string> {
-    let pdfPath = "";
-    const html = fs.readFileSync( path.join( __dirname, "./pdfTemplate.html" ), "utf8" );
-    const { testresult, appointmentid, reqUrl } = data.data;
-    let testResultBool = false;
+async function pdfTemplate(data: any): Promise<string> {
+  let pdfPath = "";
+  const html = fs.readFileSync(
+    path.join(__dirname, "./pdfTemplate.html"),
+    "utf8"
+  );
+  const { testresult, appointmentid, reqUrl, birthdate, testdatetime } =
+    data.data;
+  let testResultBool = false;
 
-    const qr64 = QR.drawImg( `${reqUrl}`, {
-        typeNumber: 4,
-        errorCorrectLevel: 'M',
-        size: 500
-    } );
+  const qr64 = QR.drawImg(`${reqUrl}`, {
+    typeNumber: 4,
+    errorCorrectLevel: "M",
+    size: 500,
+  });
 
-    console.log( testresult, reqUrl );
+  console.log(testresult, reqUrl, birthdate);
 
-    switch ( testresult ) {
-        case "negative":
-            testResultBool = false;
-            break;
-        case "positive":
-            testResultBool = true;
-        default:
-            break;
-    }
+  switch (testresult) {
+    case "negative":
+      testResultBool = false;
+      break;
+    case "positive":
+      testResultBool = true;
+    default:
+      break;
+  }
 
-    const options = {
-        format: "A4",
-        orientaion: "portrait"
-    };
+  const options = {
+    format: "A4",
+    orientaion: "portrait",
+  };
 
-    const document = {
-        html: html,
-        data: { ...data, testResultBool, qr: qr64 },
-        path: "./output.pdf",
-        type: "",
-    };
+  const document = {
+    html: html,
+    data: {
+      ...data,
+      normalBirthdate: new Date(testdatetime).getDate(),
+      testResultBool,
+      qr: qr64,
+      qrCodeHref: reqUrl,
+    },
+    path: "./output.pdf",
+    type: "",
+  };
 
-    await pdf.create( document, options )
-        .then( ( res: any ) => {
-            pdfPath = res.filename;
-            console.log( res.filename );
-        } )
-        .catch( ( error: any ) => {
-            console.error( error );
-        } );
-    return pdfPath;
-};
+  console.log(document.data);
+
+  await pdf
+    .create(document, options)
+    .then((res: any) => {
+      pdfPath = res.filename;
+      console.log(res.filename);
+    })
+    .catch((error: any) => {
+      console.error(error);
+    });
+  return pdfPath;
+}
 
 export default pdfTemplate;
